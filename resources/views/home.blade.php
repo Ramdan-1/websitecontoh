f635e8532a09958d1fb215c4e595ba7cc57ec748
Ramdan-1 <arr70265@gmail.com>
2022-06-23 09:59:37 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;800&display=swap" rel="stylesheet">
+    <style>
+        body{
+            background-color: #F2D1D1;
+        }
+        .hero{
+            font-family: 'Poppins', sans-serif;;
+            color: #1f1f1f;
+            margin-top: 5%;
+            background-color: #FFE6E6;
+            width: 500px;
+            border-radius: 20px;
+        }
+        .content{
+            width: 1100px;
+            margin: auto;
+            margin-top: 30px;
+            box-shadow: 0 10px 10px #DAEAF1;
+            background-color: #ffffff;    
+            border-radius: 10px;
+            font-family: 'Poppins';
+        }
+
+        h4 {
+            font-family: "Poppins";
+            background-color: #C6DCE4;
+            color: #1f1f1f;
+            margin-top: 2%;
+            width: 500px;
+            border-radius: 20px;
+
+        }
+        h5 {
+            font-family: "Poppins";
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h1 class="hero p-3">Welcome to our website</h1>
+        <h4 class="p-4">This website is created to fullfill Mr Andri's Task</h4>
+        <h4 class="p-2">First member</h4>
+    </center>
+    <div class="content p-3 text-center">
+        <h3>Nama    :Ramdan Abdul Rahman</h3>
+        <h3>Jenis kelamin   : Laki-laki</h3>
+        <h3>TTL     : Garut, 7 Februari 2003</h3>
+        <h3>Hobi    : PSan</h3>
+        <h3>Jurusan : Sistem Informasi Jaringan dan Aplikasi</h3>
+    </div>
+    <center><h4 class="p-2">Second member</h4></center>
+    <div class="content p-3 text-center">
+        <h3>Nama    :Alpa Rizki Ramadhan</h3>
+        <h3>Jenis kelamin   : Laki-laki</h3>
+        <h3>TTL     : Garut, 26 November 2004</h3>
+        <h3>Hobi    : Futsal Sepak Bola</h3>
+        <h3>Jurusan : Sistem Informasi Jaringan dan Aplikasi</h3>
+    </div>
+    <    <center><h4 class="p-2">Third member</h4></center>
+    <div class="content p-3 text-center">
+        <h3>Nama    :Ropikin</h3>
+        <h3>Jenis kelamin   : Laki-laki</h3>
+        <h3>TTL     : Garut, 27 Januari 2003</h3>
+        <h3>Hobi    : Nonton</h3>
+        <h3>Jurusan : Sistem Informasi Jaringan dan Aplikasi</h3>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
